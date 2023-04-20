4ce7523439693ff64ac8d242edab83ef1c241d0a
Your Name <you@example.com>
2023-04-20 15:42:12 -0500

    mocking the worker

---
+++ lib/app.ex
@@ -0,0 +1,26 @@
+# lib/chat/application.ex
+defmodule Chat.Application do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Cluster.Supervisor, [topologies(), [name: BackgroundJob.ClusterSupervisor]]},
+      #HordeTaskRouter.HordeRegistry,
+      {Task.Supervisor, name: Chat.TaskSupervisor}
+    ]
+
+    opts = [strategy: :one_for_one, name: Chat.Supervisor]
+    Supervisor.
+    (children, opts)
+  end
+
+  defp topologies do
+    [
+      background_job: [
+        strategy: Cluster.Strategy.Gossip
+      ]
+    ]
+  end
+end
