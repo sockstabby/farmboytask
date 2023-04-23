dacca4196e1dd121fea3557b1206ebd816664a16
Your Name <you@example.com>
2023-04-23 16:02:58 -0500

    dealing with args better

--- lib/first_distributed_task.ex
+++ lib/first_distributed_task.ex
@@ -3,17 +3,20 @@ defmodule FirstDistributedTask do
   alias Phoenix.PubSub
 
 
-  def hello(args) do
-    send_message("starting task")
+  def hello(roomid, args) do
+    send_message("starting task", roomid)
 
-    IO.puts("YAWN #{args}")
+
+
+    IO.puts("YAWN #{IO.inspect(args)}")
     Process.sleep(1000 * 60)
     IO.puts("awake now")
 
-    send_message("ending task")
+    send_message("ending task", roomid)
   end
 
-  defp send_message(msg) do
-    PubSub.broadcast(:tasks, "user:123", {:task_update, %{id: 123, msg: msg}})
+  defp send_message(msg, roomid) do
+    me = Atom.to_string(node)
+    PubSub.broadcast(:tasks, "user:123", {:task_update, %{id: 123, msg: msg, node: me, roomid: roomid}})
   end
 end
