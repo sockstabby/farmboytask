707aee64bcf67571790a9c098689e40c68acdeba
Your Name <you@example.com>
2023-04-27 14:25:22 -0500

    added new origin node parm

--- lib/first_distributed_task.ex
+++ lib/first_distributed_task.ex
@@ -3,20 +3,18 @@ defmodule FirstDistributedTask do
   alias Phoenix.PubSub
 
 
-  def hello(roomid, args) do
-    send_message("starting task", roomid)
-
-
+  def hello(roomid, origin_node, args) do
+    send_message("starting task", roomid, origin_node)
 
     IO.puts("YAWN #{IO.inspect(args)}")
     Process.sleep(1000 * 60)
     IO.puts("awake now")
 
-    send_message("ending task", roomid)
+    send_message("ending task", roomid, origin_node)
   end
 
-  defp send_message(msg, roomid) do
+  defp send_message(msg, roomid, origin_node) do
     me = Atom.to_string(node)
-    PubSub.broadcast(:tasks, "user:123", {:task_update, %{id: 123, msg: msg, node: me, roomid: roomid}})
+    PubSub.broadcast(:tasks, "user:123", {:task_update, %{id: 123, msg: msg, node: me, roomid: roomid, origin_node: origin_node}})
   end
 end
